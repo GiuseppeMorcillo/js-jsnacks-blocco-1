ed07273cb804b4a48496f8e92e9ce8bee2e3f11e
GiuseppeMorcillo <beppebeppe18@gmail.com>
2025-02-27 12:27:03 +0100

    feat: terzo esercizio completato

---
+++ jsnack3/script.js
@@ -0,0 +1,8 @@
+/*Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+let somma = 0;
+for (let i = 0; i < 10; i++) {
+    let numero = parseFloat(prompt(`Inserisci i numeri ${i + 1}:`));
+    somma += numero;
+}
+console.log(somma);
